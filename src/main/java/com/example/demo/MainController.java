40be1b48e59c02928099ba39e9a9f26b3797e7aa
April Z <4961561+apzr@users.noreply.github.com>
2021-09-16 15:10:33 +0800

    Keycloak 默认认证

--- src/main/java/com/example/demo/MainController.java
+++ src/main/java/com/example/demo/MainController.java
@@ -1,34 +1,39 @@
 package com.example.demo;
 
+import org.keycloak.adapters.springsecurity.token.KeycloakAuthenticationToken;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpHeaders;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClient;
+import org.springframework.security.oauth2.client.OAuth2AuthorizedClientService;
+import org.springframework.security.oauth2.client.authentication.OAuth2AuthenticationToken;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+import org.springframework.web.reactive.function.client.ClientRequest;
+import org.springframework.web.reactive.function.client.ExchangeFilterFunction;
+import reactor.core.publisher.Mono;
 
 import javax.annotation.security.RolesAllowed;
 import java.security.Principal;
-import java.util.HashMap;
-import java.util.Map;
 
 @Controller
 public class MainController {
 
     @GetMapping("/")
-    public String index() {
+    public String index(Model model) {
         return "index";
     }
 
-    @GetMapping("/userinfo1")
-    public String userinfo1(Principal principal) {
-   	    Map<String, Object> map = new HashMap<String, Object>();
-    	map.put("username", principal.getName());
-    	System.out.println("Debug: the username is " + principal.getName());
-        return "userinfo1";
-    }
-
+    @RolesAllowed("ROLE_VIEWER")
     @GetMapping("/userinfo")
-    public String userinfo(Principal principal) {
-   	    Map<String, Object> map = new HashMap<String, Object>();
-    	map.put("username", principal.getName());
-    	System.out.println("Debug: the username is " + principal.getName());
+    public String userinfo(Model model, KeycloakAuthenticationToken authentication) {//KeycloakAuthenticationToken
+        model.addAttribute("userName", authentication.getName());
+        model.addAttribute("credentials", authentication.getAccount().getKeycloakSecurityContext().getTokenString());
+
         return "userinfo";
     }
+
 }
---
+++ src/main/java/com/example/demo/config/LogoutProvider.java
@@ -0,0 +1,13 @@
+package com.example.demo.config;
+
+/**
+ * LogoutProvider
+ * <p>
+ * <br/>
+ *
+ * @author apr
+ * @date 2021/09/16 15:02:53
+ **/
+public class LogoutProvider{
+
+}
